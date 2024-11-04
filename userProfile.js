0176b195c0b307ac79f5b1be2371e51156b815cf
Pabsyyy <christiangalinato1218@gmail.com>
2024-11-04 18:24:57 +0800

    initial commit

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Christian Alford M. Galinato";
+let Age = "19";
+let favoriteNumber = "12";
+let favoriteColor = "black";
+
+console.log(fullName)
+console.log(Age)
+console.log(favoriteNumber)
+console.log(favoriteColor)
